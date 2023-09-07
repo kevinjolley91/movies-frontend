192c8a158008a285e2bbe7e9217fddf69a228dc1
Kevin Jolley <jolley427@gmail.comm>
2023-09-07 15:27:44 -0400

    failure

--- src/MoviesShow.jsx
+++ src/MoviesShow.jsx
@@ -1,17 +1,5 @@
 /* eslint-disable react/prop-types */
-import axios from "axios";
-import { useState } from "react";
-
 export function MoviesShow(props) {
-  const [favorites, setFavorites] = useState([]);
-
-  const handleCreateFavorite = (params) => {
-    console.log("handleCreateFavorite");
-    axios.post("http://localhost:3000/favorites.json", params).then((response) => {
-      setFavorites([...favorites, response.data]);
-    });
-  };
-
   return (
     <div>
       <h1>Movie Information</h1>
@@ -19,7 +7,6 @@ export function MoviesShow(props) {
       <img src={props.movie.image_url} width="150" height="225" alt={props.movie.name} />
       <p>Description: {props.movie.description}</p>
       <p>Category: {props.movie.category}</p>
-      <button onClick={() => handleCreateFavorite(props.movie.id)}>Add to Favorites</button>
     </div>
   );
 }
