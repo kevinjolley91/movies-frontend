56bc7b337a4e09b94685c2a17c2de5329e2837e9
Kevin Jolley <jolley427@gmail.comm>
2023-09-13 15:49:04 -0400

    removed random space from header

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,8 +6,8 @@ export function Header() {
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <Link to="/">All Movies</Link> | <Link to="/movies/new">New Movie</Link> |
-        <Link to="favorites/index"> Your Favorites</Link> | <Link to="/signup"> Signup</Link> |{" "}
-        <Link to="/login">Login</Link> | <LogoutLink>Logout</LogoutLink> | <Link to="/about">About</Link>
+        <Link to="favorites/index"> Your Favorites</Link> | <Link to="/signup"> Signup</Link> |
+        <Link to="/login"> Login</Link> | <LogoutLink>Logout</LogoutLink> | <Link to="/about">About</Link>
       </div>
     </nav>
   );
