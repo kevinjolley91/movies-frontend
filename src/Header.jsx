c942d0ba25aa3baf7993effd3043dc544fd2b2b9
Kevin Jolley <jolley427@gmail.comm>
2023-09-16 12:35:56 -0400

    simplified header file

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,12 +3,10 @@ import { LogoutLink } from "./Logout";
 
 export function Header() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <Link to="/">All Movies</Link> | <Link to="/movies/new">New Movie</Link> |
-        <Link to="favorites/index"> Your Favorites</Link> | <Link to="/signup"> Signup</Link> |
-        <Link to="/login"> Login</Link> | <LogoutLink>Logout</LogoutLink> | <Link to="/about">About</Link>
-      </div>
+    <nav className="navbar">
+      <Link to="/">All Movies</Link> | <Link to="/movies/new">New Movie</Link> |
+      <Link to="favorites/index"> Your Favorites</Link> | <Link to="/signup"> Signup</Link> |
+      <Link to="/login"> Login</Link> | <LogoutLink>Logout</LogoutLink> | <Link to="/about">About</Link>
     </nav>
   );
 }
