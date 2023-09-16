d1d857d03a4bb2d26f7b1c3665c10bf4feaa42b8
Kevin Jolley <jolley427@gmail.comm>
2023-09-16 11:45:06 -0400

    adds cards to movies index

--- src/MoviesIndex.jsx
+++ src/MoviesIndex.jsx
@@ -3,14 +3,17 @@ export function MoviesIndex(props) {
   return (
     <div>
       <h1>All Movies</h1>
-      {props.movies.map((movie) => (
-        <div key={movie.id}>
-          <h2>{movie.name}</h2>
-          <img src={movie.image_url} width="200" height="300" alt={movie.name} />
-          <br />
-          <button onClick={() => props.onShowMovie(movie)}>More info</button>
-        </div>
-      ))}
+      <div className="card-group">
+        {props.movies.map((movie) => (
+          <div key={movie.id} className="card">
+            <img className="card-img-top" src={movie.image_url} width="200" height="300" alt={movie.name} />
+            <div className="card-body">
+              <h5 className="card-title">{movie.name}</h5>
+              <button onClick={() => props.onShowMovie(movie)}>More info</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
