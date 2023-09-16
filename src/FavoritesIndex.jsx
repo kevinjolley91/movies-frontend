51ffb8d9c498bdb1f9dba6a6adce0601515d76b1
Kevin Jolley <jolley427@gmail.comm>
2023-09-16 11:51:54 -0400

    adds cards to favorites index

--- src/FavoritesIndex.jsx
+++ src/FavoritesIndex.jsx
@@ -3,14 +3,23 @@ export function FavoritesIndex(props) {
   return (
     <div>
       <h1>Your Favorites</h1>
-      {props.userFavorites.map((userFavorite) => (
-        <div key={userFavorite.id}>
-          <h2>{userFavorite.movie.name}</h2>
-          <img src={userFavorite.movie.image_url} width="200" height="300" alt={userFavorite.movie.name} />
-          <br />
-          <button onClick={() => props.onShowMovie(userFavorite.movie)}>More info</button>
-        </div>
-      ))}
+      <div className="card-group">
+        {props.userFavorites.map((userFavorite) => (
+          <div key={userFavorite.id} className="card">
+            <img
+              className="card-img-top"
+              src={userFavorite.movie.image_url}
+              width="200"
+              height="300"
+              alt={userFavorite.movie.name}
+            />
+            <div className="card-body">
+              <h2 className="card-title">{userFavorite.movie.name}</h2>
+              <button onClick={() => props.onShowMovie(userFavorite.movie)}>More info</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
