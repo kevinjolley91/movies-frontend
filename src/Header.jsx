e0d99a2df2fe34db5b555ccb3e362c00333ef1a0
Kevin Jolley <jolley427@gmail.comm>
2023-10-02 14:18:12 -0400

    makes header conditional if user is logged in or not

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom";
 import { LogoutLink } from "./Logout";
 
 export function Header() {
-  return (
+  const loggedIn = localStorage.getItem("jwt");
+
+  return loggedIn ? (
+    <nav className="navbar">
+      <Link to="/">All Movies</Link> | <Link to="/movies/new">New Movie</Link> |
+      <Link to="favorites/index"> Your Favorites</Link> | <LogoutLink>Logout</LogoutLink> |{" "}
+      <Link to="/about">About</Link>
+    </nav>
+  ) : (
     <nav className="navbar">
       <Link to="/">All Movies</Link> | <Link to="/movies/new">New Movie</Link> |
       <Link to="favorites/index"> Your Favorites</Link> | <Link to="/signup"> Signup</Link> |
-      <Link to="/login"> Login</Link> | <LogoutLink>Logout</LogoutLink> | <Link to="/about">About</Link>
+      <Link to="/login"> Login</Link>| <Link to="/about">About</Link>
     </nav>
   );
 }
