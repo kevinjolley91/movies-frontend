a7e50f0a417030f4f321197d823fb218ec7cba0a
Kevin Jolley <jolley427@gmail.comm>
2023-10-18 14:19:13 -0400

    displays info from api call

--- src/MoviesShow.jsx
+++ src/MoviesShow.jsx
@@ -5,9 +5,14 @@ export function MoviesShow(props) {
     <div>
       <h1>Movie Information</h1>
       <h3>{props.movie.title}</h3>
-      <img src={props.movie.image_url} width="200" height="300" alt={props.movie.name} />
-      <p>{props.movie.description}</p>
-      <p>Category: {props.movie.category}</p>
+      <img
+        src={"https://image.tmdb.org/t/p/original/" + props.movie.poster_path}
+        width="200"
+        height="300"
+        alt={props.movie.title}
+      />
+      <p>{props.movie.release_date}</p>
+      <p>{props.movie.overview}</p>
     </div>
   );
 }
