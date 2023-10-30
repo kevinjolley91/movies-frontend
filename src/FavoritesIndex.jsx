fba0da78cb6475eade80157b0041d5034cbdde27
Kevin Jolley <jolley427@gmail.comm>
2023-10-30 11:02:13 -0400

    rendering favorite index starting to work

--- src/FavoritesIndex.jsx
+++ src/FavoritesIndex.jsx
@@ -8,14 +8,14 @@ export function FavoritesIndex(props) {
           <div key={favorite.id} className="card">
             <img
               className="card-img-top"
-              src={"https://image.tmdb.org/t/p/original/" + props.favorite.movie_poster_path}
+              src={"https://image.tmdb.org/t/p/original/" + favorite.movie_poster_path}
               width="200"
               height="300"
-              alt={props.favorite.movie_title}
+              alt={favorite.movie_title}
             />
             <div className="card-body">
-              <h2 className="card-title">{props.favorite.movie_title}</h2>
-              <button onClick={() => props.onShowMovie(props.favorite)}>More info</button>
+              <h2 className="card-title">{favorite.movie_title}</h2>
+              <button onClick={() => props.onShowMovie(favorite)}>More info</button>
             </div>
           </div>
         ))}
