01bdc48a254f8cf4e703d43df2027838831c627e
Kevin Jolley <jolley427@gmail.comm>
2023-11-02 08:46:55 -0400

    updates about page further

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Map, Marker } from "pigeon-maps";
 
 export function About() {
   return (
     <div>
       <h1>About What to Watch</h1>
-      <h2>The Better IMDB</h2>
+      <h2>
+        We built What to Watch, so you don't have to wonder anymore. We're based in a small town shown on the map.
+        There's practically nothing to do here, so we binge movies to fill our time. We know what to watch.
+      </h2>
       <Map height={300} defaultCenter={[41.3978, -79.8314]} defaultZoom={11}>
         <Marker width={50} anchor={[41.3978, -79.8314]} />
       </Map>
